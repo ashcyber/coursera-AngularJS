4387417959ebc96ee5e16c1c255b1c615d4788da
Aishwarya Sharma <ishans632@gmail.com>
2018-03-24 23:00:44 +0530

    Add mod1_solution

---
+++ mod1_solution/js/app.js
@@ -0,0 +1,44 @@
+(function(){
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  function LunchCheckController($scope){
+
+
+
+    $scope.lunchBtn = function(){
+      // Clear Previous Data
+      $scope.result = "";
+
+      // Fetch Items
+      var items = $scope.lunch;
+      // If Data item is entered
+      if(items){
+        // Input Length Find
+        var items_length = items.split(',').length;
+
+        // Conditions
+        if(items_length <= 3){
+          $scope.result = 'Enjoy!';
+        }else{
+          $scope.result = 'Too much!';
+        }
+        $scope.colorValue = "green";
+        $scope.borderValue = "border: 1px solid green" ;
+
+      }
+      // No data is entered
+      else{
+        $scope.result = "Please enter data first";
+        $scope.colorValue = "red";
+        $scope.borderValue = "border: 1px solid red"   ;
+
+      }
+
+
+    }
+  };
+
+
+
+})();
