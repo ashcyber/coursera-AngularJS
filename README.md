9a560d8dd75be5420c95906dcff79ad6882e0614
Aishwarya Sharma <ishans632@gmail.com>
2018-03-30 14:42:39 +0530

    Add mod2_solution

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Single Page Web Applications with AngularJS
 
 - [mod1_solution](https://ashcyber.github.io/coursera-AngularJS/mod1_solution/index.html)
+- [mod2_solution](https://ashcyber.github.io/coursera-AngularJS/mod2_solution/index.html)
---
+++ mod2_solution/js/app.js
@@ -0,0 +1,76 @@
+(function(){
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService); 
+
+
+  // Injecting to protect from minification 
+  ToBuyController.$inject = ['$scope', 
+  'ShoppingListCheckOffService']; 
+  AlreadyBoughtController.$inject = ['$scope',
+  'ShoppingListCheckOffService']; 
+
+
+  function ToBuyController($scope, 
+    ShoppingListCheckOffService){
+    var buyList = this;
+
+    buyList.items = ShoppingListCheckOffService.getBuyItems();
+    console.log(buyList.items); 
+
+
+    buyList.buyItem = function(itemIndex){
+      ShoppingListCheckOffService.buyItem(itemIndex); 
+    }
+
+  }
+
+
+  function AlreadyBoughtController($scope, 
+    ShoppingListCheckOffService){
+    var boughtList = this;
+
+    boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+    console.log(boughtList.items); 
+
+  }
+
+
+  function ShoppingListCheckOffService(){
+    // Keep track of buy and bought items at the same time 
+    var service = this; 
+
+    var buy_items = [
+      { name: "cookies", quantity: 10 }, 
+      { name: "cola", quantity: 2 },
+      { name: "chocolate", quantity: 9 },
+      { name: "juice", quantity: 5 },
+      { name: "apple", quantity: 6 }
+    ]; 
+
+    var bought_items = []; 
+
+
+
+    service.buyItem = function(itemIndex){
+      // Remove from buy and put it in bought 
+      var item = buy_items[itemIndex]; 
+      buy_items.splice(itemIndex, 1); 
+      bought_items.push(item); 
+    }; 
+
+    // Expose buy_items array 
+    service.getBuyItems = function(){
+      return buy_items; 
+    }; 
+
+    // Expose bought_items array 
+    service.getBoughtItems = function(){
+      return bought_items; 
+    }; 
+  }
+
+})();
